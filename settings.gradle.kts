f59806ac069689d98a0c85a6a3221ab248709847
Thomas de Witt <thomasdewitt@shareforce.eu>
2024-04-05 09:29:51 +0200

    Blank screen when cancelling predictive back gesture

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://androidx.dev/snapshots/latest/artifacts/repository") }
 
     }
 }
